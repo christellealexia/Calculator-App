078e1b5b71f70b4780b4d4f9829203d7b7718ff1
Alexia Christelle <christellealexia53@gmail.com>
2024-08-01 15:55:01 +0200

    Worked on the delete button

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
-import  './App.css'
 import {useReducer} from 'react'
-import Digitbutton from "./Digitbutton"
+import  './App.css'
+import One from './One'
+import Two from './Two'
 
 export const ACTIONS
 = {
@@ -14,42 +15,93 @@ export const ACTIONS
 function reducer(state,{type,payload}){
 switch(type){
   case ACTIONS.ADD_DIGIT:
+    if(payload.digit === '0' && state.currentOperand === '0' )return state
+    if(payload.digit === '.' && state.currentOperand === '.' )return state
+    
+    return {
+      ...state,
+      currentOperand: `${state.currentOperand || ""} ${payload.digit}`,
+
+    }
+    case ACTIONS.CHOOSE_OPERATION:
+      if(state.currentOperand === null && state.previousOperand === null){
+        return state;
+      }
+      if(state.previousOperand === null){
+        return {
+          ...state,
+          operation:payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand:null,
+        }
+      }
+      return {
+        ...state,
+        previousOperand:evaluate(state),
+        operation: payload.operation,
+        currentOperand: null
+      }
+
+      function evaluate({currentOperand,previousOperand,operation}){
+        const prev = parseFloat(previousOperand)
+        const current = parseFloat(currentOperand)
+        If(isNaN(prev) || isNaN(current)) {return ""}
+        let computation = ""
+        switch(operation){
+          case " + ":
+            computation = prev + current;
+            break;
+            case " - ":
+            computation = prev - current;
+            break;
+            case " * ":
+            computation = prev * current;
+            break;
+            case " ÷":
+            computation = prev / current;
+            break;
+        }
+        return computation.toString()
+      }
+      case ACTIONS.CLEAR:
+        return {}
+        default:
+      return state;
 }
 }
+
+
 function App (){
   const[{currentOperand,previousOperand,operation},dispatch]=useReducer(reducer,{})
+  
 return (
   <div className="calculator-grid">
     <div className="output">
-      <div className="previous-operand">234567 *</div>
-      <div className="current-operand"></div>
+      <div className="previous-operand">{previousOperand} {operation}</div>
+      <div className="current-operand">{currentOperand}</div>
     </div>
-    <button className="span-two">AC</button>
+    <button className="span-two" onClick={()=> dispatch({type:ACTIONS.CLEAR})}>AC</button>
     <button>DEL</button>
-    <button>÷</button>
+    
+    <Two operation='÷' dispatch ={dispatch}/>
    
-    <Digitbutton digit={1} dispatch ={dispatch}/>
-    <Digitbutton digit={2} dispatch ={dispatch}/>
-    <Digitbutton digit={3} dispatch ={dispatch}/>
+    <One digit='1' dispatch ={dispatch}/>
+    <One digit='2' dispatch ={dispatch}/>
+    <One digit='3' dispatch ={dispatch}/>
+    <Two operation ='*' dispatch ={dispatch}/>
     
-    <button>×</button>
-    <Digitbutton digit={4} dispatch ={dispatch}/>
-    <Digitbutton digit={5} dispatch ={dispatch}/>
-    <Digitbutton digit={6} dispatch ={dispatch}/>
-   
-    <button>+</button>
-    <Digitbutton digit={7} dispatch ={dispatch}/>
-    <Digitbutton digit={8} dispatch ={dispatch}/>
-    <Digitbutton digit={9} dispatch ={dispatch}/>
-   
-    
-    <button>−</button>
-    <button>.</button>
-    <button>0</button>
-    <button className="span-two">=</button>
-    
-    
+    <One digit='4' dispatch ={dispatch}/>
+    <One digit='5' dispatch ={dispatch}/>
+    <One digit='6' dispatch ={dispatch}/>
+    <Two operation ='+' dispatch ={dispatch}/>
+    <One digit= '7' dispatch ={dispatch}/>
+    <One digit= '8' dispatch ={dispatch}/>
+    <One digit= '9' dispatch ={dispatch}/>
+    <Two operation = '-' dispatch ={dispatch}/>
+    <One digit= '.' dispatch ={dispatch}/>
+    <One digit= '0' dispatch ={dispatch}/>
 
+    <button className="span-two">=</button>
   </div>
 
 )
--- vite-project/src/One.jsx
+++ vite-project/src/One.jsx
@@ -1,5 +1,5 @@
 import {ACTIONS} from './App'
 
-export function Digitbutton({dispatch,digit}){
+export default function One({dispatch,digit}){
 return <button onClick = {() =>dispatch({type:ACTIONS.ADD_DIGIT,payload:{digit}})}>{digit}</button>
 }
